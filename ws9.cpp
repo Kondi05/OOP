43c5a2b7ffe4a8b8128a914e43964f6ace0eb966
Kondi05 <jkkondi@gmail.com>
2025-10-06 23:23:56 +1030

    Complete workshop 9 with proper exception handling

--- ws9.cpp
+++ ws9.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+
 using namespace std;
 
 typedef vector<int> Records;
@@ -11,53 +12,58 @@ class RecordsManager {
 private:
     fstream _file;
     string _filename;
-    
+
 public:
     RecordsManager(string filename) : _filename(filename) {}
-    
+
+    // Update read function with exception handling
     void read(Records &records) {
         _file.open(_filename, ios::in);
-        if (!_file.is_open()) {
-            throw runtime_error("Unable to open file");
-        }
-        
+
         try {
-            string line;
-            int lineNumber = 0;
+            if (!_file.is_open())
+            {
+                throw runtime_error("Unable to open file");
+            }
             
+            string line;
             while (getline(_file, line)) {
-                lineNumber++;
                 try {
                     int value = stoi(line);
                     records.push_back(value);
                 }
                 catch (const invalid_argument& e) {
                     cout << "invalid_argument error" << endl;
-                    throw; // Re-throw to propagate to main
+                    _file.close();
+                    throw;
                 }
                 catch (const out_of_range& e) {
                     cout << "out_of_range error" << endl;
-                    throw; // Re-throw to propagate to main
+                    _file.close();
+                    throw;
                 }
             }
+            _file.close();
         }
         catch (...) {
-            _file.close();
+            if (_file.is_open()) {
+                _file.close();
+            }
             throw;
         }
-        _file.close();
     }
 };
 
 int main(int argc, char* argv[]) {
+    Records myRecords;
+
+    string filename = "records.txt";
+    if (argc > 1) filename = argv[1];
+
     try {
-        Records myRecords;
-        string filename = "records.txt";
-        if (argc > 1) filename = argv[1];
-        
-        RecordsManager recordM(filename);
+        RecordsManager recordM(filename); 
         recordM.read(myRecords);
-        
+
         int sum = 0;
         for (int i = 0; i < myRecords.size(); i++) {
             sum += myRecords[i];
@@ -65,13 +71,8 @@ int main(int argc, char* argv[]) {
         cout << sum << endl;
     }
     catch (const exception& e) {
-        // Catch any propagated exceptions
-        return 1; // Return error code
+        return 1;
     }
-    catch (...) {
-        // Catch any other exceptions
-        return 1; // Return error code
-    }
-    
+
     return 0;
 }
